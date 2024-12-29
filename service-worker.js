79ec924142144f5e385f9d6f78cba0fc55986111
hjshdja <sainipushkar07022007@gmail.com>
2024-12-30 01:47:37 +0530

    Added service-worker.js and updated PWA setup

---
+++ service-worker.js
@@ -0,0 +1,13 @@
+self.addEventListener('install', (event) => {
+    console.log('Service Worker: Installed');
+});
+
+self.addEventListener('activate', (event) => {
+    console.log('Service Worker: Activated');
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        fetch(event.request).catch(() => new Response('You are offline.'))
+    );
+});
